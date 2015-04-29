fdc840ab4881f3575d527e91f68577d47967178e
ahou8288 <ahou8288@uni.sydney.edu.au>
2015-04-30 07:49:39 +1000

    Half implemented the arguments and functions selection

--- snapshot.c
+++ snapshot.c
@@ -6,8 +6,7 @@
 
 void getCommand(void);
 void run(char* commandName, char* userCommand);
-void BYE(void);
-
+void BYE(char* args);
 
 entry* entry_head = NULL;
 entry* entry_tail = NULL;
@@ -21,24 +20,37 @@ int main(void) {
 }
 
 void getCommand(void){
+	//Get user input
 	char userCommand[MAX_LINE_LENGTH];
 	fgets(userCommand,MAX_LINE_LENGTH,stdin);
-	char * pch;
-	pch = strtok (userCommand," ");
 	
+	//Duplicate string because original string is modified.
+	char args[MAX_LINE_LENGTH];
+	strcpy(args,userCommand);
+	
+	char * pch;
+	pch = strtok (userCommand," "); //Split string
 	if (pch != NULL)
 	{
 		//At this point send user command to get processed at run(pch,usercommand)
-		run(pch,userCommand);
+		run(pch,args);
 	}
 	
 }
 
-void run(char* commandName, char* userCommand){
-	void (*byePtr)()=BYE;
-	(*byePtr)();
+void run(char* commandName, char* args){
+	//Create a list of function names and function pointers
+	//Run the function with the arguments
+
+	char* functionName="BYE";
+	void (*byePtr)(char*)=BYE;
+	printf("%s\n",commandName);
+	if (strcmp(commandName,functionName)==0){
+		(*byePtr)(args);
+	}
 }
 
-void BYE(){
-	printf("Bye\n");
+void BYE(char* args){
+	printf("Bye function arguments :%s\n",args);
+	printf("Bye function has been run\n");
 }
