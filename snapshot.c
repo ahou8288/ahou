6825b006c96ba4f6338ceccccc834ceb1e44c4e9
ahou8288 <ahou8288@uni.sydney.edu.au>
2015-05-01 15:38:39 +1000

    Added the append function. Still debugging :(

--- snapshot.c
+++ snapshot.c
@@ -5,7 +5,7 @@
 
 #include "snapshot.h"
 
-#define NUM_FUNCTIONS 3
+#define NUM_FUNCTIONS 5
 
 //Function Definitions
 void getCommand(void);
@@ -13,6 +13,8 @@ void run(char* commandName, char* userCommand);
 void BYE(char* args);
 void SET(char* args);
 void GET(char* args);
+void append(char* args);
+void help(void);
 char* strLower(char* inString);
 
 //Snapshot 1
@@ -72,16 +74,23 @@ void run(char* commandName, char* arguments){
         myFnc[2].name="get";
         myFnc[2].ptr=&GET;
         myFnc[2].args=1;
+        myFnc[3].name="append";
+        myFnc[3].ptr=&append;
+        myFnc[3].args=1;
+        myFnc[4].name="help";
+        myFnc[4].ptr=&help;
+        myFnc[4].args=0;
 
 	// printf("Command name is %s\n",strLower(commandName));
 	for (int i=0; i<NUM_FUNCTIONS; i++){
-		// printf("%s --- %s\n",commandName,myFnc[i].name);
+		printf("%s --- %s\n",commandName,myFnc[i].name);
 		if (strcmp(strLower(commandName),myFnc[i].name)==0){
 			if (myFnc[i].args>0){
 				(myFnc[i].ptr)(arguments);
 			} else {
 				(myFnc[i].ptr)();
 			}
+			break;
 		}
 	}
 }
@@ -122,7 +131,7 @@ void SET(char* args){
 	snapPtr.prev=0;
 	snapPtr.id=1;
 
-	printf("Value put is %d.",snapPtr.entries->values->value);
+	printf("Value put is %d.\n",snapPtr.entries->values->value);
 }
 
 void GET(char* args){
@@ -130,8 +139,54 @@ void GET(char* args){
 	char command[MAX_LINE_LENGTH];
 	sscanf("%s %s",command,key);
 	if (&snapPtr.entries->values!=0){ //Where key = key!
-		printf("[%d]\n",snapPtr.entries->values->value);
+		printf("[");
+
+		//Traverse the list and print each value
+		struct value ptr;
+		ptr.next=snapPtr.entries->values->next;
+		ptr.prev=0;
+		while (ptr.next!=0){
+			printf("%d ",ptr.value);
+	                ptr.prev=ptr.next;
+        	        ptr.next=ptr.next->next;
+		}
+		printf("]\n");
+		//printf("[%d]\n",snapPtr.entries->values->value);
 	}else{
 		printf("[]\n");
 	}
+}
+
+void append(char* args){
+	char key[MAX_KEY_LENGTH];
+        int value;
+        char command[MAX_LINE_LENGTH]; //not used
+        printf("Args to append function are %s\n",args);
+	sscanf(args,"%s %s %d",command,key,&value);
+
+	printf("test 2\n");
+	//Reach the end of the list
+	struct value ptr;
+	ptr.prev=0;
+	ptr.next=snapPtr.entries->values->next;
+	
+	printf("test\n");
+
+	//printf("%d\n",ptr.next);
+	while (ptr.next!=0){
+		printf("next address\n");
+		ptr.prev=ptr.next;
+		ptr.next=ptr.next->next;
+	}
+	
+	//tme="append";
+	//        myFnc[3].ptr=&append;
+	//                myFnc[3].args=1;
+	//                mpVal->next=&newVal;
+	//newVal.prev=tmpVal;
+	//newVal.next=0;
+}
+
+void help(void){
+	printf("%s",HELP);
 }
